eb4bf26850f82a6debf6c75948ded5ef9d0cb04a
Brenda Profiro <brendaprofiro@gmail.com>
2025-09-30 17:57:04 -0300

    fix name

--- src/environments/definitions/base/tokens.ts
+++ src/environments/definitions/base/tokens.ts
@@ -281,7 +281,7 @@ export const tokens = createTokenConfig({
   meUSDC: {
     address: "0xe1ba476304255353aef290e6474a417d06e7b773",
     decimals: 18,
-    name: "Moonwell meUSDC",
+    name: "Moonwell Ecosystem USDC Vault",
     symbol: "meUSDC",
   },
 });
