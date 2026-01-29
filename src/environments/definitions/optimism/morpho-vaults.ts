3513dbf6ab9a180e4150edc49115765b4c0503ba
Brenda Profiro <brendaprofiro@gmail.com>
2026-01-29 16:49:04 -0300

    deprecate op vault and isolated markets

--- src/environments/definitions/optimism/morpho-vaults.ts
+++ src/environments/definitions/optimism/morpho-vaults.ts
@@ -10,6 +10,7 @@ export const vaults = createVaultConfig({
       multiReward: "0x2EED2b7d44E2cF64a41B6b3f78bE2Fdc56223d2B",
       campaignId:
         "0x3c5d019808525f25ae0edc7cfe5fdbce6896625281e47ee9a39cbaa6a5c2a1e1",
+      deprecated: true,
     },
   },
 });
